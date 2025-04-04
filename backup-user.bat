a8f8114410e93bfa9c00ff1783a2bda980092715
Fabian Panzer <vodan@users.noreply.github.com>
2025-04-04 12:54:04 +0200

    Initial commit save first script

---
+++ backup-user.bat
@@ -0,0 +1,29 @@
+@echo off
+SETLOCAL
+
+:: === Configuration ===
+set "SRC=C:\Users"
+set "DST=E:\Backups\Users"
+set "LOG=E:\Backups\backup_log.txt"
+
+:: === Timestamp Header ===
+echo ======================================== >> "%LOG%"
+echo Backup started at %DATE% %TIME% >> "%LOG%"
+echo ======================================== >> "%LOG%"
+
+:: === Start Robocopy ===
+robocopy "%SRC%" "%DST%" /E /Z /W:5 /R:5 /FFT /TEE /LOG+:"%LOG%" ^
+ /XD "AppData\Local" "AppData\LocalLow" "AppData\Temp" ^
+     "AppData\Roaming\Microsoft\Windows\Recent" ^
+     "AppData\Roaming\Microsoft\Windows\Themes\CachedFiles" ^
+     "AppData\Roaming\Adobe" ^
+ /XF "NTUSER.DAT" "ntuser.dat.LOG1" "ntuser.dat.LOG2"
+
+:: === Completion Message ===
+echo ======================================== >> "%LOG%"
+echo Backup finished at %DATE% %TIME% >> "%LOG%"
+echo ======================================== >> "%LOG%"
+
+echo Backup complete. See log file at: %LOG%
+pause
+ENDLOCAL
